de1b4447294ec3f8bab48ae56c916c7647fdc922
BigmanGhark <iadtboxingsociety@gmail.com>
2024-02-05 20:20:59 +0000

    Single instance lock

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"context"
 	"embed"
+	"strings"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+func (a *App) onSecondInstanceLaunch(secondInstanceData options.SecondInstanceData) {
+	secondInstanceArgs := secondInstanceData.Args
+
+	println("user opened second instance", strings.Join(secondInstanceData.Args, ","))
+	println("user opened second from", secondInstanceData.WorkingDirectory)
+	runtime.WindowUnminimise(*&a.ctx)
+	runtime.Show(*&a.ctx)
+	go runtime.EventsEmit(*&a.ctx, "launchArgs", secondInstanceArgs)
+}
 
 func main() {
 	// Create an instance of the app structure
@@ -37,6 +49,11 @@ func main() {
 		OnStartup:        onStartup,
 		Bind: []interface{}{
 			app,
+		},
+
+		SingleInstanceLock: &options.SingleInstanceLock{
+			UniqueId:               "e3984e09-28dc-4e3d-b70a-45e961589cdc",
+			OnSecondInstanceLaunch: app.onSecondInstanceLaunch,
 		},
 	})
 
